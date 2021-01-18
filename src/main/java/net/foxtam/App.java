ce384b96b73577f61d24f810caf46e1a2c5682c7
Foxtam <foxtam7@ya.ru>
2021-01-18 20:30:30 +0300

    Stage 1/5

--- src/main/java/net/foxtam/App.java
+++ src/main/java/net/foxtam/App.java
@@ -1,10 +1,21 @@
 package net.foxtam;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        int mapSize = scanner.nextInt();
+        int seed = scanner.nextInt();
+
+        new App().run(mapSize, seed);
+    }
+
+    private void run(int mapSize, int seed) {
+        UniverseMap universeMap = new UniverseMap(mapSize, seed);
+        System.out.println(universeMap);
     }
 }
